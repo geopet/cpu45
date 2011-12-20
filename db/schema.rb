30e37345650ef84faa2fb0ea02d1c7ffaafcc586
Clifford T. Matthews <ctm@devctm.com>
2011-12-19 19:41:26 -0700

    Set up User to use devise.

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,37 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110905192916) do
+ActiveRecord::Schema.define(:version => 20110928003817) do
 
   create_table "users", :force => true do |t|
-    t.string   "login",          :null => false
-    t.string   "email",          :null => false
-    t.string   "time_zone",      :null => false
-    t.string   "first_name",     :null => false
+    t.string   "login",                                                 :null => false
+    t.string   "time_zone",                                             :null => false
+    t.string   "first_name",                                            :null => false
     t.string   "middle_initial"
-    t.string   "last_name",      :null => false
+    t.string   "last_name",                                             :null => false
     t.date     "birthday"
     t.string   "sex"
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email"
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
