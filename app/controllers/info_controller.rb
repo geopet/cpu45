b42f40180cfcf04b2e7ce6851db7e915f76bc582
Clifford T. Matthews <ctm@devctm.com>
2012-12-25 17:24:49 -0700

    Uses asset pipeline.  Has cleaner paths.  Removed static pages.

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,19 +1,4 @@
 class InfoController < ApplicationController
-  def overview
-  end
-
-  def aid_stations
-  end
-
-  def course_description
-  end
-
-  def directions
-  end
-
-  def rules
-  end
-
-  def schedule
-  end
+  caches_page :overview, :aid_stations, :course_description, :directions,
+              :rules, :schedule
 end
