e0ca6d4508d97969c2e0998589b8a190effc1529
Clifford T. Matthews <ctm@devctm.com>
2012-12-29 19:45:13 -0700

    We redirect away from www.cpu45 and cpu45.com to simple cpu45.org.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Cpu45::Application.routes.draw do
+
+  # If we get a leading "www.", delete it.  If we get a trailing ".com",
+  # turn it into a ".org".  These should be rare enough that there's no
+  # point in doing this in apache, nginx or middleware.
+
+  constraints(:host => /(^www\.cpu45)|(cpu45\.com$)/) do
+    match "(*x)" => redirect { |_, request|
+      URI.parse(request.url).tap do |url|
+        url.host.sub!(/^www\./, '')
+        url.host.sub!(/\.com$/, '.org')
+        url
+      end.to_s
+    }
+  end
+
   %w(overview aid_stations course_description directions rules
                                                        schedule).each do |page|
     get page => "info##{page}"
